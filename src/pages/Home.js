4da7913f6c17f70e832a3b90998b114b2461c529
babdurrahman78 <babdurrahman78@gmail.com>
2021-10-09 17:48:22 +0700

    chore: Fix styling home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,18 @@
 import React, {  } from 'react'
 import styled from 'styled-components'
-import calendar from '../assets/icon/calendar.png'
-import calendarTimes from '../assets/icon/calendar-times.png'
-import group from '../assets/icon/group.png'
-import wallet from '../assets/icon/account_balance_wallet.png'
-import shield from '../assets/icon/shield.png'
-import workab from '../assets/workab-bg.png'
-import meeting from '../assets/meeting.png'
+import visiting from '../assets/visiting.png'
+import clock from '../assets/clock.png'
+import girl from '../assets/girl-laptop.png'
 
 const StyledContainer = styled.div`
-    width: 100%;
+    box-sizing: border-box;
+    background-color: #FFFFFF;
+    max-width: 1440px;
+    margin: auto;
+    padding: 50px 87px 0 87px;
+    display: flex;
+    flex-direction: column;
+    gap: 100px;
 ` 
 
 const StyledParagraph = styled.p`
@@ -20,82 +23,36 @@ const StyledTitle = styled(StyledParagraph)`
     font-size: 70px;
     color: #686868;
     font-weight: bold;
-    margin: 40px 0 0 45px;
 `
 
 const StyledDesc = styled(StyledParagraph)`
-    font-size: 40px;
-    margin: 0 0 0 45px;
-`
-
-const StyledConatainerIcon = styled.div`
-    display: flex;
-    margin: 22px 0 0 45px;
-`
-
-const StyledIcon = styled.i`
-    width: 100px;
-    height: 100px;
-    background-size: cover;
-    display: block;
-    margin-right: 77px;
-`
-
-const StyledIconCalendar = styled(StyledIcon)`
-     background-image: url(${calendar});
-`
-
-const StyledIconCalenderTimes = styled(StyledIcon)`
-    background-image: url(${calendarTimes});
-`
-
-const StyledIconGroup = styled(StyledIcon)`
-    background-image: url(${group});
-`
-
-const StyledIconWallet = styled(StyledIcon)`
-    background-image: url(${wallet});
-`
-
-const StyledIconShield = styled(StyledIcon)`
-    background-image: url(${shield});
-`
-
-const StyledWorkab = styled.img`
-    position: absolute;
-    width: 875px;
-    height: 714px;
-    top: 180px;
-    left: 433px;
-    z-index: -1;
+    font-size: 25px;
 `
 
 const StyledButton = styled.button`
     display: block;
-    width: 286px;
-    height: 112px;
+    width: 262px;
+    height: 111px;
+    margin-top: 16px;
     background-color: #577BA8;
     border: none;
     border-radius: 15px;
-    margin: 191px auto 359px;
     cursor: pointer;
 `
 
 const StyledParagraphButton = styled.p`
-    font-size: 30px;
+    font-size: 25px;
     color: #FFFFFF;
 `
 
 const StyledRow = styled.div`
     display: flex;
-    height: 256px;
     margin-bottom: 10px;
 `
 
 const StyledFeatureContainer = styled.div`
     color: #000000;
-    padding-left: 45px;
-    flex: 1;
+    width: 640px;
 `
 
 const StyledFeatureTitle = styled.p`
@@ -103,7 +60,8 @@ const StyledFeatureTitle = styled.p`
 `
 
 const StyledFeatureDesc = styled.p`
-    font-size: 20px; 
+    font-size: 20px;
+    font-weight: 400px; 
     border-top: 3px solid black;
 `
 
@@ -111,55 +69,64 @@ const StyledFeatureImageContainer = styled.div`
     flex: 1;
 `
 
+const StyledLeftImage = styled.img`
+    width: 350px;
+    display: block;
+    margin-right: auto;
+`
+
+const StyledRightImage = styled.img`
+    width: 350px;
+    display: block;
+    margin-left: auto;
+`
+
 const StyledFeatureImage = styled.img`
     display: block;
     margin: auto;
-    width: 471px;
-    height: 256px;
 `
 
 export default function Home() {
     return (
         <StyledContainer>
-            <StyledWorkab src={workab}/>
-            <StyledTitle>
-                SELAMAT DATANG DI WORKAB
-            </StyledTitle>
-            <StyledDesc>
-                Absen kantor mudah dan cepat secara online
-            </StyledDesc>
-            <StyledConatainerIcon>
-                <StyledIconCalendar />
-                <StyledIconCalenderTimes />
-                <StyledIconGroup />
-                <StyledIconWallet />
-                <StyledIconShield />
-            </StyledConatainerIcon>
-            <StyledButton>
-                <StyledParagraphButton>
-                    Selengkapnya
-                </StyledParagraphButton>
+            <StyledRow>
+                <StyledFeatureContainer>
+                    <StyledTitle>
+                        SELAMAT DATANG DI WORKAB
+                    </StyledTitle>
+                    <StyledDesc>
+                        Absen kantor mudah dan cepat secara online
+                    </StyledDesc>
+                    <StyledButton>
+                    <StyledParagraphButton>
+                        Selengkapnya
+                    </StyledParagraphButton>
             </StyledButton>
+                </StyledFeatureContainer>
+                <StyledFeatureImageContainer>
+                    <StyledRightImage src={girl} alt="girl" />
+                </StyledFeatureImageContainer>
+            </StyledRow>
             <StyledRow>
                 <StyledFeatureContainer>
                     <StyledFeatureTitle>
-                        Fitur Meeting
+                        Fitur Attendance
                     </StyledFeatureTitle>
                     <StyledFeatureDesc>
                     Workab mempunyai fitur untuk melakukan absensi via perangkat dan dapat melihat riwayat absensi via website dimanapun dan kapanpun
                     </StyledFeatureDesc>
                 </StyledFeatureContainer>
                 <StyledFeatureImageContainer>
-                    <StyledFeatureImage src={meeting} alt="meeting" />
+                    <StyledRightImage src={clock} alt="clock" />
                 </StyledFeatureImageContainer>
             </StyledRow>
             <StyledRow>
                 <StyledFeatureImageContainer>
-                    <StyledFeatureImage src={meeting} alt="meeting" />
+                    <StyledLeftImage src={visiting} alt="visiting" />
                 </StyledFeatureImageContainer>
                 <StyledFeatureContainer>
                     <StyledFeatureTitle>
-                        Fitur Meeting
+                        Fitur Visiting
                     </StyledFeatureTitle>
                     <StyledFeatureDesc>
                     Workab mempunyai fitur untuk melakukan absensi via perangkat dan dapat melihat riwayat absensi via website dimanapun dan kapanpun
