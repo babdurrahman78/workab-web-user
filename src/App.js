a85e43c5bc3c93b62a7d6589d1bf134e54e003d7
babdurrahman78 <babdurrahman78@gmail.com>
2021-10-12 14:57:39 +0700

    chore: Remove package-lock.json

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import Navbar from "./components/Navbar";
-import Sidebar from "./components/Sidebar";
 import Attendance from "./pages/Attendance";
 import Visiting from "./pages/Visiting";
 
